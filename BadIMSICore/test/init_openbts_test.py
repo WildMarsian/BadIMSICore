33846f3266fc69225833331f0be30ffdae29ead1
johnny <jwankutk@etud.u-pem.fr>
2016-02-02 16:15:53 +0100

    transform openbts init into a class

--- BadIMSICore/test/init_openbts_test.py
+++ BadIMSICore/test/init_openbts_test.py
@@ -1,14 +1,18 @@
 import unittest
 
-from src.init_openbts import init_sipauthserve
+from src.init_openbts import InitOpenBTS
+
 class init_openbts_test(unittest.TestCase):
+
     def init_sipauthserve_success_test(self):
-        sortie = init_sipauthserve()
-        print(sortie)
+        o = InitOpenBTS()
+        sortie = o.init_sipauthserve()
         self.assertEquals(True,sortie)
+
     def init_sipauthserve_fail_test(self):
-        sortie = init_sipauthserve()
-        print(sortie)
+        o = InitOpenBTS()
+        sortie = o.init_sipauthserve()
         self.assertEquals(False,sortie)
+
 if __name__ == '__main__':
     unittest.main()
