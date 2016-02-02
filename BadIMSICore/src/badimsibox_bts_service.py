c33321e331ee7d82d1d8e119e592b6810ed0a575
TTAK <arthur.bd@wanadoo.fr>
2016-02-02 13:31:42 +0100

    Implementation of the service using popen

--- BadIMSICore/src/badimsibox_bts_service.py
+++ BadIMSICore/src/badimsibox_bts_service.py
@@ -1,15 +1,22 @@
 import os
+import subprocess
+from multiprocessing import Process, Pipe
 
-filename = "testfile"
+std_out_file = "teststdout"
+std_in_file = "teststdin"
 
 try:
-    os.mkfifo(filename)
+    os.mkfifo(std_out_file)
+    os.mkfifo(std_in_file)
 except RuntimeError as err:
     print("Failed to create FIFO: %s" % err)
 else:
-    fifo = open(filename, 'w')
-    # write stuff to fifo
-    print >> fifo, "hello"
-    fifo.close()
-    os.remove(filename)
+    fifo_out = open(std_out_file, 'w')
+    fifo_in = open(std_in_file, 'r')
+    p = subprocess.popen(["bc"], stdout=fifo_out, stdin=fifo_in, stderr=fifo_out)
+    p.comunicate()
 
+fifo_out.close()
+fifo_in.close()
+os.remove(std_in_file)
+os.remove(std_out_file)
