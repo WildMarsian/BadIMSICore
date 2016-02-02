ff3458194dc934608496e9082850a5b7dc0e143f
Zak <zdjeblou@etud.u-pem.fr>
2016-02-02 14:44:41 +0100

    Refactory Bad SMS scripts for naming

--- BadIMSICore/src/bad_sms_interceptor.py
+++ BadIMSICore/src/bad_sms_interceptor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3.4
 
-#Usage: BadSMSInterceptor.py -i <inputlog>
+#Usage: bad_sms_interceptor.py -i <inputlog>
 
-from PyTail import PyTail
+from pytail import PyTail
 import sys
 import getopt
 
@@ -13,11 +13,11 @@ def main():
         opts, args = getopt.getopt(sys.argv[1:], "hi", ["help", "input="])
     except getopt.GetoptError as err:
         print(err)
-        print("Usage: BadSMSInterceptor.py -i <input>")
+        print("Usage: bad_sms_interceptor.py -i <input>")
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print("Usage: BadSMSInterceptor.py -i <input>")
+            print("Usage: bad_sms_interceptor.py -i <input>")
             sys.exit(0)
         elif opt in ("-i", "--input"):
             input_log = args[0]
