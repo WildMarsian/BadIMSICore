8423fa73bbf2d57d0e0b9a76ee3a1b5a4be158e1
johnny <jwankutk@etud.u-pem.fr>
2016-02-02 13:47:31 +0100

    creating test for init_openbts.py to be continued

--- BadIMSICore/test/init_openbts_test.py
+++ BadIMSICore/test/init_openbts_test.py
@@ -1,15 +1,15 @@
 import unittest
 
-from src import init_openbts
+import src.init_openbts as openbts
 
 class init_openbts_test(unittest.TestCase):
 
     def init_sipauthserve_success_test(self):
-        sortie = init_openbts.init_sipauthserve()
+        sortie = openbts.init_sipauthserve()
         self.assertEquals(True,sortie)
 
     def init_sipauthserve_fail_test(self):
-        sortie = init_openbts.init_sipauthserve()
+        sortie = openbts.init_sipauthserve()
         self.assertEquals(False,sortie)
 
 if __name__ == '__main__':
