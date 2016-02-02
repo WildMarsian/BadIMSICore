d682bcd5efb1bbf63310ff4f62582717f26cefee
johnny <jwankutk@etud.u-pem.fr>
2016-02-02 10:27:24 +0100

    All the functions required to init the openbts service and launching openbts service with transceiver

--- BadIMSICore/src/init_openbts.py
+++ BadIMSICore/src/init_openbts.py
@@ -5,7 +5,6 @@ import subprocess
 
 #to launch openBTS we need at least that sipauthserve and smqueue
 
-
 # launch the sipauthserve service
 def init_sipauthserve():
     sortie = subprocess.call(args="start sipauthserve",shell=True, stdout=subprocess.PIPE)
@@ -13,9 +12,23 @@ def init_sipauthserve():
         return 1
     return 0
 
+# stop the sipauthserve service
+def stop_sipauthserve():
+    sortie = subprocess.call(args="stop sipauthserve",shell=True, stdout=subprocess.PIPE)
+    if sortie == 0:
+        return True
+    return False
+
 # launch the smqueue service
 def init_smqueue():
     sortie = subprocess.call(args="start smqueue",shell=True, stdout=subprocess.PIPE)
+    if sortie == 0:
+        return True
+    return False
+
+# stop the smqueue service
+def stop_smqueue():
+    sortie = subprocess.call(args="stop smqueue",shell=True, stdout=subprocess.PIPE)
     if sortie == 0:
         return True
     return False
@@ -35,11 +48,25 @@ def init_openbts():
     return False
 
 
-# Main
-first = init_sipauthserve()
-second = init_smqueue()
-# checking if the two sub modules are enabled
-if first == True and second == True:
-    third = init_transceiver()
-    fourth = init_openbts()
+
+def launch_openbts():
+    # Stop all the services first
+    stop_sipauthserve()
+    stop_smqueue()
+    # Launch the two required services
+    first = init_sipauthserve()
+    second = init_smqueue()
+    # checking if the two sub modules are enabled
+    if first == True and second == True:
+        third = init_transceiver()
+        fourth = init_openbts()
+        if(third==True and fourth==True):
+            # we can launch openbts command here
+            return True
+        else:
+            return False
+
+
+launch_openbts()
+
 
