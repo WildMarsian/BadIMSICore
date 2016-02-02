3c517e7eb3a163d649b18bf88cd83f51ebfccbc9
Zak <zdjeblou@etud.u-pem.fr>
2016-02-02 12:04:29 +0100

    BadMSInterceptor, competed reading and parsing. Displays all the messages if not read before

--- BadIMSICore/src/BadMSInterceptor.py
+++ BadIMSICore/src/BadMSInterceptor.py
@@ -2,6 +2,19 @@
 
 from PyTail import PyTail
 
-for line in PyTail("/var/log/syslog"):
+# We use a counter to skip every other line since the log duplicates SMQueue entries
+count = 0
+
+# There is 13 entries in SMQueue line log. With a list the last index is 12
+last_index_of_smqueue_line = 12
+
+for line in PyTail("smqueue.txt"):
+    count += 1
+    line_words = line.split(" ")
+    # If it's a line containing "Decoded", it means it's a SMQueue log entry
     if "Decoded" in line:
-        print(line)
+        # If the count is even, that means it's a duplicate
+        if count % 2 == 0:
+            # We remove that extra carrier return for pretty printing
+            parsed_entry = line_words[last_index_of_smqueue_line][:-1]
+            print(parsed_entry)
