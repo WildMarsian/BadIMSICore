5a1f74fc063df00998c737b351237ecacfa93a35
johnny <jwankutk@etud.u-pem.fr>
2016-02-02 18:17:12 +0100

    testing done

--- BadIMSICore/test/init_openbts_test.py
+++ BadIMSICore/test/init_openbts_test.py
@@ -7,9 +7,28 @@ from init_openbts import InitOpenBTS
 
 class init_openbts_test(TestCase):
 
-    def test_init_sipauthserve_failed(self):
+    def test_init_sipauthserve_fail(self):
         o = InitOpenBTS()
-        self.assertEquals(False,o.init_sipauthserve())
+        self.assertEquals(False, o.init_sipauthserve())
 
+    def test_stop_sipauthserve_fail(self):
+        o = InitOpenBTS()
+        self.assertEquals(False, o.stop_sipauthserve())
+
+    def test_init_smqueue_fail(self):
+        o = InitOpenBTS()
+        self.assertEquals(False,o.init_smqueue())
+
+    def test_stop_smqueue_fail(self):
+        o = InitOpenBTS()
+        self.assertEquals(False,o.stop_smqueue())
+
+    def test_init_transceiver_fail(self):
+        o = InitOpenBTS()
+        self.assertEquals(False,o.init_transceiver())
+
+    def test_init_openbts_fail(self):
+        o = InitOpenBTS()
+        self.assertEquals(False,o.init_openbts())
 if __name__ == '__main__':
     unittest.main()
