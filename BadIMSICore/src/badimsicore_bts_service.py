c007f30ef65b5036264c3bb7e2056536918c77ba
TTAK <arthur.bd@wanadoo.fr>
2016-02-03 17:01:59 +0100

    Used OpenBtsDo instead of OpenBtsCLI

--- BadIMSICore/src/badimsicore_bts_service.py
+++ BadIMSICore/src/badimsicore_bts_service.py
@@ -5,26 +5,19 @@ from daemon import Daemon
 
 
 class BadimsicoreBtsService(Daemon):
+    
+    def __init__(self, pidfile):
+        super(BadimsicoreBtsService, self).__init__(pidfile)
 
     def run(self):
-        p = subprocess.Popen("bc", stdout=self.stdout, stdin=self.stdin, stderr=self.stderr)
-        p.communicate()
+        subprocess.Popen(["/OpenBts/OpenBts"])
 
     @staticmethod
     def get_badimsicore_bts_service(exec_context):
-        std_out_file = os.path.join(exec_context, "bbts_stdout")
-        std_in_file = os.path.join(exec_context, "bbts_stdin")
-        std_err_file = os.path.join(exec_context, "bbts_stderr")
         pid_file = os.path.join(exec_context, "badimsicore_bts.pid")
+        return BadimsicoreBtsService(pid_file)
 
-        if not os.path.isfile(pid_file):
-            try:
-                os.mkfifo(std_out_file)
-                os.mkfifo(std_in_file)
-                os.mkfifo(std_err_file)
-                return BadimsicoreBtsService(pid_file, std_out_file, std_in_file, std_err_file)
-
-            except IOError as ioErr:
-                print("IO error : unable tu create named pipe (might be impossible on windows) : %s\n", ioErr)
-
-            return BadimsicoreBtsService(pid_file, std_out_file, std_in_file, std_err_file)
+    @staticmethod
+    def sendCommand(command):
+        p = subprocess.Popen(["/OpenBts/OpenBtsDo", "-c", command])
+        return p.communicate()
