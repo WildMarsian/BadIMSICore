dcec38281fd9081b8f1a9f6532b771d50a74658e
johnny <jwankutk@etud.u-pem.fr>
2016-02-03 18:43:56 +0100

    args conditions

--- BadIMSICore/src/badimsicore_sniffing_xml_parsing.py
+++ BadIMSICore/src/badimsicore_sniffing_xml_parsing.py
@@ -4,32 +4,36 @@
 import xml.etree.ElementTree as ET
 import sys
 
-xmlfilename = sys.argv[1]
-tree = ET.parse(xmlfilename)
-space = ' '
+if len(sys.argv) == 2:
+    xmlfilename = sys.argv[1]
+    tree = ET.parse(xmlfilename)
+    space = ' '
+    for node in tree.iter('proto'):
+        name = node.attrib.get('name')
+        if name == "gsm_a.ccch":
+            system_info_type = node.attrib.get('showname')
+            print(system_info_type)
+            for info in node.iter('field'):
+                # case 3 and 4
+                showname = info.attrib.get('showname')
+                show = info.attrib.get('show')
 
-for node in tree.iter('proto'):
-    name = node.attrib.get('name')
-    if name == "gsm_a.ccch":
-        system_info_type = node.attrib.get('showname')
-        print(system_info_type)
-        for info in node.iter('field'):
-            # case 3 and 4
-            showname = info.attrib.get('showname')
-            show = info.attrib.get('show')
+                if "List of ARFCNs" in show:
+                    if info.attrib.get('show') is not None:
+                        print(space,info.attrib.get('show'))
 
-            if "List of ARFCNs" in show:
-                if info.attrib.get('show') is not None:
-                    print(space,info.attrib.get('show'))
+                if "Cell Identity - CI" in show:
+                    if info.attrib.get('show') is not None:
+                        print(space,info.attrib.get('show'))
 
-            if "Cell Identity - CI" in show:
-                if info.attrib.get('show') is not None:
-                    print(space,info.attrib.get('show'))
+                if "Location Area Identification (LAI) -" in show:
+                    if info.attrib.get('showname') is not None:
+                        print(space,info.attrib.get('showname'))
 
-            if "Location Area Identification (LAI) -" in show:
-                if info.attrib.get('showname') is not None:
-                    print(space,info.attrib.get('showname'))
+                    for lac in info.iter('field'):
+                        if lac.attrib.get('showname') is not None:
+                            print(space, space,lac.attrib.get('showname'))
 
-                for lac in info.iter('field'):
-                    if lac.attrib.get('showname') is not None:
-                        print(space, space,lac.attrib.get('showname'))
+else:
+    print("You must put a xml file in args")
+    print("badimsicore_sniffing_xml_parsing.py <xml-file>")
