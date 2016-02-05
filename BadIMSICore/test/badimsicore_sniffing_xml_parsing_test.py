9e8d9c6cc7033f5e42ef22b5c0044833b0009f26
johnny <jwankutk@etud.u-pem.fr>
2016-02-05 15:11:04 +0100

    renaming and creating tests for xml parsing

---
+++ BadIMSICore/test/badimsicore_sniffing_xml_parsing_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from unittest import TestCase
+from badimsicore_sniffing_xml_parsing import is_valid_extension
+from badimsicore_sniffing_xml_parsing import parse_xml_file
+
+
+class XMLParsingTest(TestCase):
+    def test_file_format_failed(self):
+        return self.assertEquals(False, is_valid_extension('myfile.txt', '.xml'))
+
+if __name__ == '__main__':
+    unittest.main()
+
