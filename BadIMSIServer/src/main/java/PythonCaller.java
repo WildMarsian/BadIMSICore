d53247e6f890dbe03bd58bf774fb7005f3457017
johnny <jwankutk@etud.u-pem.fr>
2016-02-07 22:08:16 +0100

    python caller with args

--- BadIMSIServer/src/main/java/PythonCaller.java
+++ BadIMSIServer/src/main/java/PythonCaller.java
@@ -4,27 +4,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+
 /**
  * Python Caller, for
  */
 public class PythonCaller {
 
-    private String[] command;
+    private String program = "python";
+    private String[] scriptPath;
 
-    public PythonCaller(String scriptPath) {
-        this.command = new String[4];
-        this.command[0] = "python";
-        this.command[1] = scriptPath;
-        this.command[2] = "-i";
-        this.command[3] = "smqueue.txt";
+    public PythonCaller(String[] scriptPath) {
+        this.scriptPath = scriptPath;
     }
 
     public void process() throws IOException {
         Runtime rt = Runtime.getRuntime();
-        //Process pr = rt.exec(this.command);
-        String cmd = this.command[0] + " " + this.command[1];
-        Process pr = rt.exec(command);
-        System.out.println(cmd);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(program);
+        sb.append(" ");
+        for (int i = 0;i<scriptPath.length;i++) {
+            sb.append(scriptPath[i]);
+            sb.append(" ");
+        }
+        System.out.println(sb.toString());
+
+        Process pr = rt.exec(sb.toString());
         BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
         String line;
         while ((line = bfr.readLine()) != null) {
@@ -36,17 +41,38 @@ public class PythonCaller {
         URL location = PythonCaller.class.getProtectionDomain().getCodeSource()
                 .getLocation();
         String path = location.getFile();
-
         return new File(path).getParent();
     }
 
     public static void main(String[] args) {
-        System.out.println(getCleanPath());
-        PythonCaller pc = new PythonCaller("badimsicore_sms_interceptor.py");
-        try {
-            pc.process();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        if(args.length == 0) {
+            noArgsError();
         }
+
+        if(args.length > 0) {
+            System.out.println(getCleanPath());
+            PythonCaller pc = new PythonCaller(args);
+            try {
+                pc.process();
+
+            } catch (IOException e) {
+                errorOnScript(args);
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void noArgsError() {
+        System.out.println("No arguments were found");
+    }
+
+    private static void errorOnScript(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (String arg : args) {
+            sb.append(arg+" ");
+        }
+        System.out.println("Error on the script : no script found for "+sb.toString());
     }
 }
