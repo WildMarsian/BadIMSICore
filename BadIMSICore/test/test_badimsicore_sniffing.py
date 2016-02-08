08ce3e4b30439f9d03404407ef969e4c5101d9a0
nicolas <ndossa09@etud.u-pem.fr>
2016-02-08 18:17:27 +0100

    Update resources directory and tests

--- BadIMSICore/test/test_badimsicore_sniffing.py
+++ BadIMSICore/test/test_badimsicore_sniffing.py
@@ -3,6 +3,7 @@ from badimsicore_sniffing import read_from_pcap, redirect_to_xml, write_to_xml, 
 
 
 class BadimsicoreSniffingTest(TestCase):
+
     def test_input_not_in_pcap_format(self):
         input_file = 'foo.notpcap'
         ret = is_valid_extension(input_file, '.pcap')
@@ -14,14 +15,11 @@ class BadimsicoreSniffingTest(TestCase):
         self.assertFalse(ret)
 
     def test_read_from_pcap(self):
-        input_file = '/home/nicolas/Bureau/capture-test1.pcap'
+        input_file = 'resources/clean/capture.pcap'
         read_from_pcap(input_file, 'eth0', 'gsmtap.chan_type == 1')
 
     def test_write_to_xml(self):
-        input_file = '/home/nicolas/Bureau/capture-test1.pcap'
-        output_file = '/home/nicolas/Bureau/capture-test1.xml'
+        input_file = 'resources/clean/capture.pcap'
+        output_file = 'resources/capture.xml'
         write_to_xml(input_file, output_file, 'eth0', 'gsmtap.chan_type == 1')
 
-
-#if __name__ == '__main__':
-#    unittest.main()
