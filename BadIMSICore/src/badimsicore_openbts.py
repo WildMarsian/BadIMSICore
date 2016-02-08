b4fe6aab0208371e2b1aa36657043dcf8d4b013a
TTAK <arthur.bd@wanadoo.fr>
2016-02-08 11:52:40 +0100

    Config is working

--- BadIMSICore/src/badimsicore_openbts.py
+++ BadIMSICore/src/badimsicore_openbts.py
@@ -3,7 +3,10 @@ import os
 import subprocess
 from daemon import Daemon
 
-
+"""
+This class is used to control the openbts service
+You need to construct the new instance using the method get_badimsicore_bts_service(exec_context)
+"""
 class BadimsicoreBtsService(Daemon):
     
     def __init__(self, pidfile):
@@ -11,12 +14,18 @@ class BadimsicoreBtsService(Daemon):
 
     def run(self):
         subprocess.Popen(["/OpenBts/OpenBts"])
-
+    """
+    BadimsicoreBtsService factory
+    :exec_context a string corresponding to the path of the context (the place where the pid file is stored)
+    """
     @staticmethod
     def get_badimsicore_bts_service(exec_context):
         pid_file = os.path.join(exec_context, "badimsicore_bts.pid")
         return BadimsicoreBtsService(pid_file)
 
+    """BadimsicoreBtsService send a command to openbts
+    :command a list of string corresponding to the command fragment (split(" "))
+    """
     @staticmethod
     def send_command(command):
         openbts=["/OpenBts/OpenBtsDo", "-c"]
