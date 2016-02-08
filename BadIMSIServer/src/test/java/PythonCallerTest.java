96ac1632589d7fcd06bc8d7b91ccfd71a3ee9c5a
johnny <jwankutk@etud.u-pem.fr>
2016-02-08 12:15:07 +0100

    Creating Test for Python Caller

---
+++ BadIMSIServer/src/test/java/PythonCallerTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by johnny on 08/02/16.
+ */
+public class PythonCallerTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testNoArgs() {
+        PythonCaller pc = new PythonCaller(null);
+    }
+
+    @Test
+    public void testCommandFailed() throws IOException {
+        String[] args = new String[1];
+        args[0] = "../../BadIMSICore/badimsicore_openbts_init.py";
+        PythonCaller pc = new PythonCaller(args);
+        int exitValue = pc.process();
+        assertNotEquals(exitValue, 0);
+    }
+
+}
