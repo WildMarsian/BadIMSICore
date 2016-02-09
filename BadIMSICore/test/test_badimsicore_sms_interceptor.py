8bdda92112f8fefdad3eb52c8a3fbc7a4bf197c4
nicolas <ndossa09@etud.u-pem.fr>
2016-02-09 14:49:53 +0100

    Update test for sms interceptor

--- BadIMSICore/test/test_badimsicore_sms_interceptor.py
+++ BadIMSICore/test/test_badimsicore_sms_interceptor.py
@@ -4,22 +4,31 @@ import os
 from unittest import TestCase
 from badimsicore_sms_interceptor import BadSMSInterceptor
 
+exec_path = os.path.dirname(__file__)
+filename = os.path.join(exec_path, 'resources/clean/smqueue.txt')
+offset_file = os.path.abspath(filename)
+
 
 class BadSMSInterceptorTest(TestCase):
+    def setUp(self):
+        self.bsi = BadSMSInterceptor()
+        self.exec_path = os.path.dirname(__file__)
+        filename = os.path.join(self.exec_path, 'resources/clean/smqueue.txt')
+        self.input_file = os.path.abspath(filename)
 
-    @unittest.skipIf(os.path.exists("../src/smqueue.txt.offset"), "Offset exists, skipping test")
+    @unittest.skipIf(os.path.exists(offset_file), "Offset exists, skipping test")
     def test_read_no_duplicate(self):
-        bsi = BadSMSInterceptor()
-        returned_list = bsi.intercept("../src/smqueue.txt")
+        #bsi = BadSMSInterceptor()
+        returned_list = self.bsi.intercept(self.input_file)
         size = list(returned_list).__len__()
         self.assertEquals(5, size)
         pass
 
-    @unittest.skipUnless(os.path.exists("../src/smqueue.txt.offset"), "Offset exists, skipping test")
+    @unittest.skipUnless(os.path.exists(offset_file), "Offset exists, skipping test")
     def test_no_entries_after_first_read(self):
-        bsi = BadSMSInterceptor()
-        bsi.intercept("../src/smqueue.txt")
-        returned_list = bsi.intercept("../src/smqueue.txt")
+        #bsi = BadSMSInterceptor()
+        self.bsi.intercept(self.input_file)
+        returned_list = self.bsi.intercept(self.input_file)
         size = returned_list.__len__()
         self.assertEqual(0, size)
         pass
