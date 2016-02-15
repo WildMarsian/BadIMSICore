79b26f3663ea5b128bd1557e3dd2b9cd12f1749a
TTAK <arthur.bd@wanadoo.fr>
2016-02-15 09:48:08 +0100

    Merge branch 'beta' of https://github.com/WarenUT/BadIMSIBox
    # Conflicts:
    #	BadIMSICore/src/badimsicore_openbts.py

--- BadIMSICore/src/badimsicore_openbts.py
+++ BadIMSICore/src/badimsicore_openbts.py
@@ -1,35 +1,51 @@
 
 import os
 import subprocess
-from daemon import Daemon
 
 """
 This class is used to control the openbts service
 You need to construct the new instance using the method get_badimsicore_bts_service(exec_context)
 """
+class BadimsicoreBtsService:
 
+    def start(self):
+        """
+        Start the daemon
+        """
+        subprocess.Popen(["start openbts"])
 
-class BadimsicoreBtsService(Daemon):
-    
-    def __init__(self, pidfile):
-        super(BadimsicoreBtsService, self).__init__(pidfile)
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        subprocess.Popen(["stop openbts"])
 
-    def run(self):
-        subprocess.Popen(["/OpenBts/OpenBts"])
-    """
-    BadimsicoreBtsService factory
-    :exec_context a string corresponding to the path of the context (the place where the pid file is stored)
-    """
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        subprocess.Popen(["restart openbts"])
+
+    def status(self):
+        """
+        return the status of the daemon
+        """
+
     @staticmethod
     def get_badimsicore_bts_service(exec_context):
+        """
+        BadimsicoreBtsService factory
+        :exec_context a string corresponding to the path of the context (the place where the pid file is stored)
+        """
         pid_file = os.path.join(exec_context, "badimsicore_bts.pid")
         return BadimsicoreBtsService(pid_file)
 
-    """BadimsicoreBtsService send a command to openbts
-    :command a list of string corresponding to the command fragment (split(" "))
-    """
+
     @staticmethod
     def send_command(command):
+        """BadimsicoreBtsService send a command to openbts
+        :command a list of string corresponding to the command fragment (split(" "))
+        """
         openbts=["/OpenBts/OpenBtsDo", "-c"]
         openbts.extend(command)
         p = subprocess.Popen(openbts)
