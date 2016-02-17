f08a7a9ca5f958a6c2bbba8adc455d0c52bb2427
TTAK <arthur.bd@wanadoo.fr>
2016-02-17 15:41:43 +0100

    Fixed some error in badimsicore_listen

--- BadIMSICore/src/badimsicore_sniffing_toxml.py
+++ BadIMSICore/src/badimsicore_sniffing_toxml.py
@@ -117,7 +117,7 @@ def redirect_to_xml(output_xml_filename, iface, net_filter, duration=10):
                 pargs.extend(['-R', net_filter])
 
             proc = subprocess.Popen(pargs, stdout=output)
-            return proc.communicate()
+            #return proc.communicate()
 
     # Problem while opening the output file
     except IOError as err:
