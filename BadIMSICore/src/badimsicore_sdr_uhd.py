d40b3b225f2ff35e9fd68ca418309e68fc79cb13
WarenUT <thibaut.j.garcia@gmail.com>
2016-02-18 13:06:24 +0100

    Structure correction in Driver loading script and begin modifications on UHD loading script

--- BadIMSICore/src/badimsicore_sdr_uhd.py
+++ BadIMSICore/src/badimsicore_sdr_uhd.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3.4
 
+from badimsicore_sdr_driver import BadIMSICoreSdrDriver
+import usb.core
+
 class BadIMSICoreUHDDriver(BadIMSICoreSdrDriver):
-    def int(self):
-        # All here
+    def init_bts(self, sdr_id):
+        device = core.find(idVentor=0x2500, idProduct=sdr_id)
+        if device is None:
+            raise ValueError("Device not found")
+        
+        with open("output.txt", "w") as target_file:
+            print("Device found", file=target_file)
+
+        
+
+if __name__ == '__main__':
+    uhdHandler = BadIMSICoreUHDDriver()
+    uhdHandler.init(0x0020)
+        
 
 # WarenUT
 # vérifier connexion usb uhd lsusb
