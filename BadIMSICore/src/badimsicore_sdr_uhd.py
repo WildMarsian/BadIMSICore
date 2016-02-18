8c301b774de608383d87f8a35175ca0155e40d9d
WarenUT <thibaut.j.garcia@gmail.com>
2016-02-18 10:04:31 +0100

    Rename sdr-driver script to match commom names and creating uhd driver

---
+++ BadIMSICore/src/badimsicore_sdr_uhd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.4
+
+class BadIMSICoreUHDDriver(BadIMSICoreSdrDriver):
+    
+
+# vérifier connexion usb uhd lsusb
+# on/off sur usb
+# => attention au port && ID du uhd
+
+
+
+# vérifier présence b210
+# télécharger si nécessaire avec ihd donw
+# uhd_usrp_probe et gestion erreurs avec boucle et cpt => retour utilisateur
+
