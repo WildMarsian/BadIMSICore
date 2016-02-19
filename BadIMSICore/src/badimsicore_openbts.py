06f94f82d0dd37e1e5e848f730964466bb2d4160
johnny <jwankutk@etud.u-pem.fr>
2016-02-19 14:38:21 +0100

    integration of  openbts config

--- BadIMSICore/src/badimsicore_openbts.py
+++ BadIMSICore/src/badimsicore_openbts.py
@@ -4,6 +4,8 @@ import subprocess
 import argparse
 import time
 from badimsicore_openbts_init import InitOpenBTS
+from bts import BTS
+
 from badimsicore_openbts_config import BadimsicoreBtsConfig
 """
 This class is used to control the openbts service
@@ -11,25 +13,24 @@ You need to construct the new instance using the method get_badimsicore_bts_serv
 """
 class BadimsicoreBtsService:
         
-    def start(self, operator=None, ci=None, lac=None, mnc=None, mcc=None, message_registration=None):
+    def start(self, ci=None, lac=None, mnc=None, mcc=None, message_registration=None):
         #Stop openbts services
         self.stop()        
         #SDR
+
         #Config OpenBTS.db
-        if operator
-            
-        #Start openbts services
-        InitOpenBTS.init_sipauthserve()
-        InitOpenBTS.init_smqueue()
-        InitOpenBTS.init_transceiver()
-        time.sleep(7)
-        InitOpenBTS.init_openbts()
+        if ci and lac and mnc and mcc:
+            bts = BTS(mcc, mnc, lac, ci)
+            # inject bts params into OpenBTS
+            openbtsdb = '../test/resources/clean/OpenBTS.db'
+            badimsicore_bts_config = BadimsicoreBtsConfig(openbtsdb)
+            badimsicore_bts_config.update_badimsicore_bts_config(bts)
 
     def stop(self):
         InitOpenBTS.stop_openbts()
         InitOpenBTS.stop_smqueue()
         InitOpenBTS.stop_sipauthserve()    
-    
+
     @staticmethod
     def send_command(command):
         """BadimsicoreBtsService send a command to openbts
@@ -50,18 +51,19 @@ def main():
     #Subparser start_parser
     start_parser = subparsers.add_parser('start', help='Start openbts')
     start_parser.set_defaults(func=service.start)
-    start_parser.add_argument('-o', '--operator', dest='operator', help='A String of the gsm operator')
+    # start_parser.add_argument('-o', '--operator', dest='operator', help='A String of the gsm operator')
     start_parser.add_argument('-i', '--ci',  dest='ci', type=int, help='The ci of the cell')
     start_parser.add_argument('-l', '--lac', dest='lac', type=int, help='The lac of the cell')   
-    start_parser.add_argument('-m', '--mnc', dest='mnc', type=int, help='The Mobile Network Code of the cell')
+    start_parser.add_argument('-M', '--mnc', dest='mnc', type=int, help='The Mobile Network Code of the cell')
     start_parser.add_argument('-c', '--mcc', dest='mcc', type=int, help='The Mobile Country Code of the cell')
-    start_parser.add_argument('-m', '--message-registration', dest='message_registration', type=int, help='The message upon registration of a mobile in the fake network')    
+    start_parser.add_argument('-m', '--message-registration', dest='message_registration', help='The message upon registration of a mobile in the fake network')
     #Subparser stop_parser 
     stop_parser = subparsers.add_parser('stop', help='Stop openbts')
     stop_parser.set_defaults(func=service.stop)
-    
+
+    args = parser.parse_args()
     if args.subparser_name == 'start':    
-        args.func(args.operator, args.ci, args.lac, args.mnc, args.mcc, args.message_registration)
+        args.func(args.ci, args.lac, args.mnc, args.mcc, args.message_registration)
     elif args.subparser_name == 'stop':
         args.func()
 
