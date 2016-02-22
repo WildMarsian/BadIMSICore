45e94ce614b43a1f2d1fd408434763af65a1c493
WarenUT <thibaut.j.garcia@gmail.com>
2016-02-22 15:00:06 +0100

    Renaming the TIMSI call script and modifing the OpenBTS argument line

---
+++ BadIMSICore/src/badimsicore_timsi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.4
+from badimsicore_openbts import BadimsicoreBtsService
+
+class BadIMSICoreTIMSI:
+    @staticmethod
+    def get_all_timsi():
+        command = ["timsi"]
+        BadimsicoreBtsService.send_command(command)
+
+if __name__ == '__main__':
+    timsi = BadIMSICoreTIMSI()
+    return timsi.get_all_timsi()
