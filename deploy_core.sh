222f29c4b895437c917a2abdd51ed2da8cd9a3e4
TTAK <arthur.bd@wanadoo.fr>
2016-02-24 11:59:49 +0100

    Fixed linking

--- deploy_core.sh
+++ deploy_core.sh
@@ -20,12 +20,12 @@ function install(){
 	sudo chmod 755 $srcdir/badimsicore_sms_sender.py        $linkdir/badimsicore_sms_sender
 	sudo chmod 755 $srcdir/badimsicore_tmsis.py             $linkdir/badimsicore_tmsis
 	
-	sudo ln -s $srcdir/badimsicore_listen.py            $linkrootdir/badimsicore_listen
-	sudo ln -s $srcdir/badimsicore_openbts.py           $linkrootdir/badimsicore_openbts
-	sudo ln -s $srcdir/airprobe_rtlsdr_non_graphical.py $linkdir/airprobe_rtlsdr_non_graphical
-	sudo ln -s $srcdir/badimsicore_sms_interceptor.py   $linkdir/badimsicore_sms_interceptor
-	sudo ln -s $srcdir/badimsicore_sms_sender.py        $linkdir/badimsicore_sms_sender
-	sudo ln -s $srcdir/badimsicore_tmsis.py             $linkdir/badimsicore_tmsis
+	sudo ln -s $linkrootdir/badimsicore_listen $srcdir/badimsicore_listen.py
+	sudo ln -s $linkrootdir/badimsicore_openbts $srcdir/badimsicore_openbts.py
+	sudo ln -s $linkdir/airprobe_rtlsdr_non_graphical $srcdir/airprobe_rtlsdr_non_graphical.py
+	sudo ln -s $linkdir/badimsicore_sms_interceptor $srcdir/badimsicore_sms_interceptor.py
+	sudo ln -s $linkdir/badimsicore_sms_sender $srcdir/badimsicore_sms_sender.py
+	sudo ln -s $linkdir/badimsicore_tmsis $srcdir/badimsicore_tmsis.py
 }
 
 function clean(){
