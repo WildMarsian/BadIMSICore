5e25467743bdda6960cbc5db6532e00e9e534c3e
TTAK <arthur.bd@wanadoo.fr>
2016-02-24 13:56:02 +0100

    Fixed linking

--- deploy_core.sh
+++ deploy_core.sh
@@ -13,12 +13,12 @@ function install(){
 	sudo cp -r ./src $coredir
 	sudo cp -r ./resources $coredir
 	
-	sudo chmod 711 $srcdir/badimsicore_listen.py            $linkrootdir/badimsicore_listen
-	sudo chmod 711 $srcdir/badimsicore_openbts.py           $linkrootdir/badimsicore_openbts
-	sudo chmod 755 $srcdir/airprobe_rtlsdr_non_graphical.py $linkdir/airprobe_rtlsdr_non_graphical
-	sudo chmod 755 $srcdir/badimsicore_sms_interceptor.py   $linkdir/badimsicore_sms_interceptor
-	sudo chmod 755 $srcdir/badimsicore_sms_sender.py        $linkdir/badimsicore_sms_sender
-	sudo chmod 755 $srcdir/badimsicore_tmsis.py             $linkdir/badimsicore_tmsis
+	sudo chmod 711 $srcdir/badimsicore_listen.py
+	sudo chmod 711 $srcdir/badimsicore_openbts.py
+	sudo chmod 755 $srcdir/airprobe_rtlsdr_non_graphical.py
+	sudo chmod 755 $srcdir/badimsicore_sms_interceptor.py
+	sudo chmod 755 $srcdir/badimsicore_sms_sender.py
+	sudo chmod 755 $srcdir/badimsicore_tmsis.py
 	
 	sudo ln -s $linkrootdir/badimsicore_listen $srcdir/badimsicore_listen.py
 	sudo ln -s $linkrootdir/badimsicore_openbts $srcdir/badimsicore_openbts.py
