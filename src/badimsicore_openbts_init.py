fc18ced45d91819553c557b88285034331586ef6
Nico-Seiya <Nico-Seiya@users.noreply.github.com>
2016-02-25 18:28:11 +0100

    Updated documentation and variables names

--- src/badimsicore_openbts_init.py
+++ src/badimsicore_openbts_init.py
@@ -1,51 +1,105 @@
-#!/usr/bin/python3.4
+#!/usr/bin/env python3.4
+
+"""
+    This module initializes OpenBTS by launching services and
+    its dependencies.
+    
+    Needed services are restarted automatically by the function
+    init_openbts().
+    
+    Services are :
+    - sipauthserve
+    - smqueue
+    - transceiver
+    - openbts
+"""
 
 import subprocess
 import time
-#to launch openBTS we need at least that sipauthserve and smqueue
+
+
+__authors__ = "Arthur Besnard, Philippe Chang, Zakaria Djebloune, Nicolas Dos Santos, Thibaut Garcia and John Wan Kut Kai"
+__maintener__ = "Arthur Besnard, Philippe Chang, Zakaria Djebloune, Nicolas Dos Santos, Thibaut Garcia and John Wan Kut Kai"
+__licence__ = "GPL v3"
+__copyright__ = "Copyright 2016, MIMSI team" 
 
 class InitOpenBTS:
 
-
-    # launch the sipauthserve service
     @staticmethod
     def init_sipauthserve():
-        sortie = subprocess.call(args="start sipauthserve",shell=True, stdout=subprocess.PIPE)
-        return sortie == 0
+        """
+            Start sipauthserve service in an other process.
+            :return: True if the process has successfully launched,
+            otherwise return False
+        """
+        exit_code = subprocess.call(args="start sipauthserve",shell=True, stdout=subprocess.PIPE)
+        return exit_code == 0
 
-    # stop the sipauthserve service
     @staticmethod
     def stop_sipauthserve():
-        sortie = subprocess.call(args="stop sipauthserve",shell=True, stdout=subprocess.PIPE)
-        return sortie == 0
+        """
+            Stop sipauthserve service.
+            :return: True if the process has successfully stopped,
+            otherwise return False
+        """
+        exit_code = subprocess.call(args="stop sipauthserve",shell=True, stdout=subprocess.PIPE)
+        return exit_code == 0
 
-    # launch the smqueue service
     @staticmethod
     def init_smqueue():
+        """
+            Start smqueue service in an other process.
+            :return: True if the process has successfully launched,
+            otherwise return False
+        """
         exit_code = subprocess.call(args="start smqueue", shell=True, stdout=subprocess.PIPE)
         return exit_code == 0
 
-    # stop the smqueue service
     @staticmethod
     def stop_smqueue():
+        """
+            Stop smqueue service.
+            :return: True if the processe has successfully stopped,
+            otherwise return False
+        """
         exit_code = subprocess.call(args="stop smqueue", shell=True, stdout=subprocess.PIPE)
         return exit_code == 0
 
-    # launch the fake bts transceiver
+
     @staticmethod
     def init_transceiver():
+        """
+            Start transceiver service in an other process.
+            :return: True if the process has successfully launched,
+            otherwise return False
+            
+            Here subprocess.Popen() is used because transceiver is
+            launched in the background, so without exit return code in
+            the nominal case.
+        """
         exit_code = subprocess.Popen(args="/OpenBTS/transceiver")
         return exit_code == 0
 
-    # launch the openbts service
     @staticmethod
     def init_openbts():
+        """
+            Start openbts service in an other process.
+            :return: True if the process has successfully launched,
+            otherwise return False
+        """
         exit_code = subprocess.call(args="start openbts", shell=True, stdout=subprocess.PIPE)
         return exit_code == 0
 
-    # stop the openbts service and fake bts transceiver
     @staticmethod
     def stop_openbts():
+        """
+            Stop openbts service in an other process.
+            :return: True if the process has successfully launched,
+            otherwise return False
+            
+            Note that transceiver service will be also stopped by calling
+            this function (openbts usage).
+        """
         exit_code = subprocess.call(args="stop openbts", shell=True, stdout=subprocess.PIPE)
         return exit_code == 0
 
@@ -59,6 +113,10 @@ def init_openbts():
     o.init_sipauthserve()
     o.init_smqueue()
     o.init_transceiver()
+    # This sleep is needed for environement
+    # reasons : on a virtual machine, initialization
+    # of transceiver service takes few seconds, so
+    # openbts need to wait.
     time.sleep(7)
     o.init_openbts()
     
