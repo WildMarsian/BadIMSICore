5042c5255cea68485ca9c303e2d483c325174dc1
Nico-Seiya <Nico-Seiya@users.noreply.github.com>
2016-02-26 11:24:54 +0100

    Updated documentation

--- src/bts.py
+++ src/bts.py
@@ -1,5 +1,6 @@
-class BTS:
-    """
+#!/usr/bin/env python3.4
+
+ """
     Class defining a complete BTS with all informations given from several packets
     The BTS is defined by :
     - LAC
@@ -7,8 +8,15 @@ class BTS:
     - Country
     - Cell ID
     - List of ARFCNs
-    """
+"""
 
+__authors__ = "Arthur Besnard, Philippe Chang, Zakaria Djebloune, Nicolas Dos Santos, Thibaut Garcia and John Wan Kut Kai"
+__maintener__ = "Arthur Besnard, Philippe Chang, Zakaria Djebloune, Nicolas Dos Santos, Thibaut Garcia and John Wan Kut Kai"
+__licence__ = "GPL v3"
+__copyright__ = "Copyright 2016, MIMSI team"
+
+
+class BTS:
     network_operators = {'01': 'Orange', '02': 'Orange', '09': 'SFR', '10': 'SFR', '11': 'SFR', '15': 'Free', '16': 'Free',
                          '20': 'Bouygues', '21': 'Bouygues'}
 
@@ -29,9 +37,19 @@ class BTS:
         return "BTS: "+self.MNC+" "+self.MCC+" "+self.LAC+" "+self.CI+" -- "+self.shortname+" "+s
 
     def __eq__(self, other):
+        """
+            Check if the current bts is equal to the another BTS.
+            A BTS is identified by a Cell Identity.
+            
+            :return : returns True if the two BTS have the same CI,
+            otherwise returns False
+        """
         return self.CI == other.CI
 
     def nice_display(self):
+        """
+            Displays nicely BTS characteristics.
+        """
         bts_string = "-> {},{},{},{},".format(self.MNC, self.MCC, self.LAC, self.CI)
         bts_string += str(self.ARFCNs).strip("[]").replace(' ', '')
         return bts_string
@@ -39,11 +57,9 @@ class BTS:
     def add_arfcns(self, arfcns):
         """
         fuse a list of arfcn with the one of the bts object
-        Args:
-            arfcns: the arfcn to add
+        :param : arfcns: the arfcn to add
 
-        Returns:
-
+        :return : None
         """
         self.ARFCNs.extend(arfcns)
         self.ARFCNs = sorted(set(self.ARFCNs))
